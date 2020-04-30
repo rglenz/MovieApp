3051b31df5b8a05d3f219c352793d4c630e7dd60
“Raymond <rglenz@calpoly.edu>
2020-04-29 18:40:23 -0700

    added Controllers

---
+++ Lab4/Lab4/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Lab4.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "My Name is " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
